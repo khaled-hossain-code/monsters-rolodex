d7fba18175cdefeedf716c9eb742f0354350ed15
khaled hossain <md.khaled.eee@gmail.com>
2019-10-08 16:30:38 +0600

    Updates

---
+++ precache-manifest.053469e617b63a458547f3125f52a97e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b3f8b1aa7fff2503be5bf10bf5a10f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1ee2905c0d8b31d3f58e",
+    "url": "/monsters-rolodex/static/css/main.6ffc3562.chunk.css"
+  },
+  {
+    "revision": "bb86858d2f4a2ebdc33a",
+    "url": "/monsters-rolodex/static/js/2.6fd3cbb0.chunk.js"
+  },
+  {
+    "revision": "1ee2905c0d8b31d3f58e",
+    "url": "/monsters-rolodex/static/js/main.02ec9c1c.chunk.js"
+  },
+  {
+    "revision": "e2cac3764d0a466c37ea",
+    "url": "/monsters-rolodex/static/js/runtime-main.4618f850.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
